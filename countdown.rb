57acb5195770b71bdcff48110c309860e26dba63
Michael Roose <mjroose@gmail.com>
2018-08-05 15:59:05 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,9 @@
 #write your code here
 
-def countdown
+def countdown(seconds_left)
+  until seconds_left < 0
+    puts "#{seconds_left} SECOND(S)!"
+    seconds_left -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
